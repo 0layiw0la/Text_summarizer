d6d115c34982e2b434a18af436a8b191a011ad56
abdulrahim ibrahim <abdulrahimibrahimolayiwola@gmail.com>
2023-11-29 04:26:17 +0100

    Add files via upload

---
+++ Text Analyzer.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 29 02:00:07 2023
+
+@author: USER
+"""
+
+import streamlit as st
+from textly import generate_summary
+from textly import sentiment
+
+positive_style = "color: green; font-weight: bold;"
+negative_style = "color: red; font-weight: bold;"
+def main():
+    
+    st.title('Text Summarizer App')
+
+    # User input text
+    user_input = st.text_area("Enter Text to Summarize")
+
+    # Check if user input is not empty
+    if user_input:
+        # Generate summary using the function
+        summary = generate_summary(user_input)
+
+        # Display the generated summary
+        st.subheader("Generated Summary:")
+        st.write(summary)
+        # Button to trigger sentiment analysis
+    if st.button("Perform Sentiment Analysis"):
+        # Perform sentiment analysis using the sentiment function
+        sentiment_result = sentiment(user_input)
+        if sentiment_result =='Positive':
+            style = positive_style
+        elif sentiment_result == "Negative" :
+            style = negative_style
+        else: 
+            style = ""
+
+        # Display the sentiment analysis result
+        st.subheader("Sentiment Analysis Result:")
+        st.markdown(f"**Sentiment:** <span style='{style}'>{sentiment_result}</span>", unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    main()
---
+++ textly.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 29 01:46:52 2023
+
+@author: USER
+"""
+import nltk
+
+def generate_summary(text):
+    import string
+    from heapq import nlargest
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    
+    text = str(text)
+    sent_list = nltk.sent_tokenize(text)
+    if len(sent_list) > 12:
+        length = int(round(len(sent_list) / 7, 0))
+    else:
+        length = 1
+
+    nopuch = [char for char in text if char not in string.punctuation]
+    nopuch = "".join(nopuch)
+
+    processed_text = [word for word in nopuch.split() if word.lower() not in nltk.corpus.stopwords.words('english')]
+
+    word_freq = {}
+    for word in processed_text:  
+        if word not in word_freq:  
+            word_freq[word] = 1
+        else: 
+            word_freq[word] += 1
+
+    max_freq = max(word_freq.values())
+    for word in word_freq:
+        word_freq[word] = word_freq[word] / max_freq
+
+    sent_score = {}
+    for sent in sent_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_freq:
+                if sent not in sent_score:
+                    sent_score[sent] = word_freq[word]
+                else:
+                    sent_score[sent] += word_freq[word]
+
+   # Use nlargest on sentences instead of length
+    summary_sents = nlargest(length, sent_score, key=sent_score.get)
+    summary = " ".join(summary_sents)
+
+    return summary
+def sentiment(text):
+    nltk.download('vader_lexicon')
+    from nltk.sentiment import SentimentIntensityAnalyzer
+    text = str(text)
+    sia = SentimentIntensityAnalyzer()
+    sent = sia.polarity_scores(text)
+    if sent['neu'] > 0.8:
+        sentiment_label = 'Neutral'
+    elif sent['pos'] > 0.1 and (sent['pos'] > sent['neg']):
+        sentiment_label = 'Positive'
+    elif (sent['neg'] > 0.1) and (sent['neg'] > sent['pos']):
+        sentiment_label = 'Negative'
+    else:
+        sentiment_label = 'Neutral'
+    return sentiment_label
+
+        
+    
