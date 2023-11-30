09e7b2c52f2b088e63b3a7f9a4e10f19ec324a61
abdulrahim ibrahim <abdulrahimibrahimolayiwola@gmail.com>
2023-11-30 18:19:14 +0100

    Update textly.py

--- textly.py
+++ textly.py
@@ -2,53 +2,13 @@ import nltk
 def summarize(text):
     from transformers import pipeline
     text = str(text)
-    summarizer = pipeline('summarization')
-    short_text = summarizer(text,max_length = 300, min_length=20,do_sample=True)
-    return short_text
-    
-def generate_summary(text):
-    import string
-    from heapq import nlargest
-    nltk.download('stopwords')
-    nltk.download('punkt')
-    
-    text = str(text)
-    sent_list = nltk.sent_tokenize(text)
-    if len(sent_list) > 12:
-        length = int(round(len(sent_list) / 10, 0))
+    if len(text.split(" ")) > 1000:
+        return "Input too long"
     else:
-        length = 1
-
-    nopuch = [char for char in text if char not in string.punctuation]
-    nopuch = "".join(nopuch)
-
-    processed_text = [word for word in nopuch.split() if word.lower() not in nltk.corpus.stopwords.words('english')]
-
-    word_freq = {}
-    for word in processed_text:  
-        if word not in word_freq:  
-            word_freq[word] = 1
-        else: 
-            word_freq[word] += 1
-
-    max_freq = max(word_freq.values())
-    for word in word_freq:
-        word_freq[word] = word_freq[word] / max_freq
-
-    sent_score = {}
-    for sent in sent_list:
-        for word in nltk.word_tokenize(sent.lower()):
-            if word in word_freq:
-                if sent not in sent_score:
-                    sent_score[sent] = word_freq[word]
-                else:
-                    sent_score[sent] += word_freq[word]
-
-   # Use nlargest on sentences instead of length
-    summary_sents = nlargest(length, sent_score, key=sent_score.get)
-    summary = " ".join(summary_sents)
-
-    return summary
+        summarizer = pipeline('summarization')
+        short_text = summarizer(text,max_length = 300, min_length=20,do_sample=True)
+        return short_text
+    
 def sentiment(text):
     nltk.download('vader_lexicon')
     from nltk.sentiment import SentimentIntensityAnalyzer
