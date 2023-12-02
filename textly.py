55401908fa541967d64c8d45420096246bf12c31
abdulrahim ibrahim <abdulrahimibrahimolayiwola@gmail.com>
2023-12-02 18:28:50 +0100

    Update textly.py

--- textly.py
+++ textly.py
@@ -6,7 +6,7 @@ import streamlit as st
 def load_model():
     return pipeline('summarization')
 # Function to summarize text
-def summarize(text):
+def summarize(text,max,min):
     text = text.replace('.','.<eos>')
     text = text.replace('?','?<eos>')
     text = text.replace('!','!<eos>')
@@ -28,7 +28,7 @@ def summarize(text):
     summarizer = load_model()
     summary = []
     for i in chunks:
-        sumr = summarizer(i,max_length=300,min_length=90,do_sample=True)[0]['summary_text']
+        sumr = summarizer(i,max_length=max,min_length=min,do_sample=True)[0]['summary_text']
         summary.append(sumr)
     return ' '.join(summary)
 
