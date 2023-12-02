59088bd643f1f37167572c97b753716b66ffd493
abdulrahim ibrahim <abdulrahimibrahimolayiwola@gmail.com>
2023-12-02 22:06:44 +0100

    Update TextAnalyzer.py

--- TextAnalyzer.py
+++ TextAnalyzer.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from textly import sentiment,summarize
+from textly import sentiment,summarize,extract_text_from_txt,extract_text_from_docx,extract_text_from_pdf
 
 positive_style = "color: green; font-weight: bold;"
 negative_style = "color: red; font-weight: bold;"
@@ -10,6 +10,19 @@ def main():
         short = st.button("Brief overview")
     with col2:
         long = st.button("Full summary")
+    with col3:
+         uploaded_file = st.file_uploader("Choose a file", type=["txt", "docx", "pdf"])
+    if uploaded_file is not None:
+        file_extension = uploaded_file.name.split(".")[-1]
+
+        if file_extension == "txt":
+            text = extract_text_from_txt(uploaded_file)
+
+        elif file_extension == "docx":
+            text = extract_text_from_docx(uploaded_file)
+
+        elif file_extension == "pdf":
+            text = extract_text_from_pdf(uploaded_file)
     user_input = st.text_area('Enter text here')
     
     # Button to trigger summarization
@@ -20,12 +33,18 @@ def main():
             # Display the generated summary
             st.subheader("Generated Summary:")
             st.write(summary)
+        elif uploaded_file is not None:
+            summary = summarize(text,max_value= 120, min_value = 20)
+            st.write(summary)
     if long:
         # Check if user input is not empty
         if user_input:
             summary = summarize(user_input, max_value= 350, min_value = 80)
             # Display the generated summary
             st.subheader("Generated Summary:")
+            st.write(summary)
+        elif uploaded_file is not None:
+            summary = summarize(text,max_value= 350, min_value = 80)
             st.write(summary)
         # Button to trigger sentiment analysis
     if st.button("Perform Sentiment Analysis"):
