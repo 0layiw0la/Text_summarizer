079fdfc895e7374e009b8c913d6086363861ea93
abdulrahim ibrahim <abdulrahimibrahimolayiwola@gmail.com>
2023-12-02 19:44:11 +0100

    Update TextAnalyzer.py

--- TextAnalyzer.py
+++ TextAnalyzer.py
@@ -4,22 +4,24 @@ from textly import sentiment,summarize
 positive_style = "color: green; font-weight: bold;"
 negative_style = "color: red; font-weight: bold;"
 def main():
-    
+    if 'max_value' not in st.session_state:
+        st.session_state['max_value'] = 250
+        st.session_state['min_vale'] = 50
     st.title('Text Summarizer App')
     s = st.button('short')
+    if s:
+        st.session_state.max_value = 120
+        st.session_state.min_value = 20
     l = st.button('Long')
+    if l:
+        st.session_state.max_value = 350
+        st.session_state.min_value = 80  
     user_input = st.text_area('Enter text here')
     # Button to trigger summarization
     if st.button("Generate Summary"):
         # Check if user input is not empty
         if user_input:
-            if s:           
-                # Generate summary using the function
-                summary = summarize(user_input,120,20)
-            elif l:
-                summary = summarize(user_input,350,80)
-            else:
-                summary = summarize(user_input)
+            summary = summarize(user_input,st.session_state.max_value,st.session_state.min_value)
 
             # Display the generated summary
             st.subheader("Generated Summary:")
