09241ab412dc5b87d0e46b8adaf18b8dbbafff1a
abdulrahim ibrahim <abdulrahimibrahimolayiwola@gmail.com>
2023-12-02 19:02:52 +0100

    Update TextAnalyzer.py

--- TextAnalyzer.py
+++ TextAnalyzer.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from textly import summarize
 from textly import sentiment
-from textly import set_global_max set_global_min
+from textly import set_global_max,set_global_min
 
 positive_style = "color: green; font-weight: bold;"
 negative_style = "color: red; font-weight: bold;"
