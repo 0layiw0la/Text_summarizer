aac472a3aaf536cd50a9097963f1c17349064281
abdulrahim ibrahim <abdulrahimibrahimolayiwola@gmail.com>
2023-12-02 19:46:24 +0100

    Update TextAnalyzer.py

--- TextAnalyzer.py
+++ TextAnalyzer.py
@@ -6,22 +6,27 @@ negative_style = "color: red; font-weight: bold;"
 def main():
     if 'max_value' not in st.session_state:
         st.session_state['max_value'] = 250
-        st.session_state['min_vale'] = 50
+        st.session_state['min_value'] = 50
+    
     st.title('Text Summarizer App')
-    s = st.button('short')
-    if s:
+    
+    short_button = st.button('Short')
+    if short_button:
         st.session_state.max_value = 120
         st.session_state.min_value = 20
-    l = st.button('Long')
-    if l:
+    
+    long_button = st.button('Long')
+    if long_button:
         st.session_state.max_value = 350
         st.session_state.min_value = 80  
+    
     user_input = st.text_area('Enter text here')
+    
     # Button to trigger summarization
     if st.button("Generate Summary"):
         # Check if user input is not empty
         if user_input:
-            summary = summarize(user_input,st.session_state.max_value,st.session_state.min_value)
+            summary = summarize(user_input, st.session_state.max_value, st.session_state.min_value)
 
             # Display the generated summary
             st.subheader("Generated Summary:")
