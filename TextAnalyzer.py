fcf1c6c3c2bd334bc5468603cb305b01d85e52f8
abdulrahim ibrahim <abdulrahimibrahimolayiwola@gmail.com>
2023-12-02 20:15:18 +0100

    Update TextAnalyzer.py

--- TextAnalyzer.py
+++ TextAnalyzer.py
@@ -1,50 +1,41 @@
 import streamlit as st
 from textly import sentiment,summarize
 
-positive_style = "color: green; font-weight: bold;"
-negative_style = "color: red; font-weight: bold;"
-def main():
-    if 'max_value' not in st.session_state:
-        st.session_state['max_value'] = 250
-        st.session_state['min_value'] = 50
-    
+#positive_style = "color: green; font-weight: bold;"
+#negative_style = "color: red; font-weight: bold;"
+def main():  
     st.title('Text Summarizer App')
-    
-    short_button = st.button('Short')
-    if short_button:
-        st.session_state.max_value = 120
-        st.session_state.min_value = 20
-    st.session_state.sync()
-    long_button = st.button('Long')
-    if long_button:
-        st.session_state.max_value = 350
-        st.session_state.min_value = 80  
-    st.session_state.sync()
     user_input = st.text_area('Enter text here')
     
     # Button to trigger summarization
-    if st.button("Generate Summary"):
+    if st.button("Brief overview"):
         # Check if user input is not empty
         if user_input:
-            summary = summarize(user_input, max_value= st.session_state.max_value, min_value = st.session_state.min_value)
-
+            summary = summarize(user_input, max_value= 120, min_value = 20)
+            # Display the generated summary
+            st.subheader("Generated Summary:")
+            st.write(summary)
+    if st.button("Comprehensive summary"):
+        # Check if user input is not empty
+        if user_input:
+            summary = summarize(user_input, max_value= 350, min_value = 80)
             # Display the generated summary
             st.subheader("Generated Summary:")
             st.write(summary)
         # Button to trigger sentiment analysis
-    if st.button("Perform Sentiment Analysis"):
-        # Perform sentiment analysis using the sentiment function
-        sentiment_result = sentiment(user_input)
-        if sentiment_result =='Positive':
-            style = positive_style
-        elif sentiment_result == "Negative" :
-            style = negative_style
-        else: 
-            style = ""
+  #  if st.button("Perform Sentiment Analysis"):
+   #     # Perform sentiment analysis using the sentiment function
+    #    sentiment_result = sentiment(user_input)
+     #   if sentiment_result =='Positive':
+      #      style = positive_style
+       # elif sentiment_result == "Negative" :
+        #    style = negative_style
+        #else: 
+         #   style = ""
 
         # Display the sentiment analysis result
-        st.subheader("Sentiment Analysis Result:")
-        st.markdown(f"**Sentiment:** <span style='{style}'>{sentiment_result}</span>", unsafe_allow_html=True)
+        #st.subheader("Sentiment Analysis Result:")
+        #st.markdown(f"**Sentiment:** <span style='{style}'>{sentiment_result}</span>", unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
