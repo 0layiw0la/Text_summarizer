c5dd76541a7daf4c4f0a6d9d68a95a824b4bbe5a
abdulrahim ibrahim <abdulrahimibrahimolayiwola@gmail.com>
2023-12-02 21:18:42 +0100

    Update TextAnalyzer.py

--- TextAnalyzer.py
+++ TextAnalyzer.py
@@ -28,19 +28,19 @@ def main():
             st.subheader("Generated Summary:")
             st.write(summary)
         # Button to trigger sentiment analysis
-  #  if st.button("Perform Sentiment Analysis"):
-   #     # Perform sentiment analysis using the sentiment function
-    #    sentiment_result = sentiment(user_input)
-     #   if sentiment_result =='Positive':
-      #      style = positive_style
-       # elif sentiment_result == "Negative" :
-        #    style = negative_style
-        #else: 
-         #   style = ""
+    if st.button("Perform Sentiment Analysis"):
+       # Perform sentiment analysis using the sentiment function
+        sentiment_result = sentiment(user_input)
+        if sentiment_result =='Positive':
+            style = positive_style
+        elif sentiment_result == "Negative" :
+            style = negative_style
+        else: 
+            style = ""
 
         # Display the sentiment analysis result
-        #st.subheader("Sentiment Analysis Result:")
-        #st.markdown(f"**Sentiment:** <span style='{style}'>{sentiment_result}</span>", unsafe_allow_html=True)
+        st.subheader("Sentiment Analysis Result:")
+        st.markdown(f"**Sentiment:** <span style='{style}'>{sentiment_result}</span>", unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
